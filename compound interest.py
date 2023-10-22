91e2a2fcb909cf1934493373df12e37bc45d85ae
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 00:38:05 +0530

    Create compound interest.py

---
+++ compound interest.py
@@ -0,0 +1,10 @@
+37.Calculate compound interest
+
+
+def compound_interest(principal, rate, time):
+    amount = principal * (pow((1 + rate / 100), time)
+                          
+CI = Amount - principal
+                          
+    print("compound interest is", CI)
+compound_interest(10000, 10.25, 5)
