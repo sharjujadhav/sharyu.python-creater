8069c7452040a7c2dc5fd190e00dfb35fadfb37f
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-22 18:43:26 +0530

    Create area of rect .py

---
+++ area of rect .py
@@ -0,0 +1,8 @@
+7. Calculate the area of a rectangle given its length and width.
+
+length = int(input("enter a length"))
+breadth = int(input("enter a breadth"))
+print(f"length is {length}")
+print(f"breadth is {breadth}")
+area = length * breadth
+print(f"area is {area}")
