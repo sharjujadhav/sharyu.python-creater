07a0f4ebac73f532c6781f557ae5c37a89b1d75b
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 01:20:10 +0530

    Update main dia ele matrix.py

--- main dia ele matrix.py
+++ main dia ele matrix.py
@@ -1,10 +1,17 @@
 45.Find the sum of the main diagonal elements of a matrix.
 
-import numpy as np
-a = np.array([[1, 2], [3, 4]])
-b = np.array([[4, 5], [6, 7]])
-print(" printing elements of first matrix")
-print(a)
-print(" printing element of second matrix")
-print("addition of two matrix")
-print(np.add(a, b))
+
+def solve(matrix):
+        m = len(matrix)
+        if m == 1: return matrix[0][0]
+        
+        count = 0
+        for i in range(m):
+            count += matrix[i][i]
+            count += matrix[i][-1 -i]
+            
+        if m % 2 ==1: count -= matrix[m // 2][m // 2]
+            
+        return count 
+matrix = [[10,5,9,6],[8,15,3,2],[3,8,12,3],[2,11,7,3],]
+print(solve(matrix))
