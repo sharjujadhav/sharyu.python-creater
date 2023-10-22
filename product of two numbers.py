0a8cb010c256d8b2b43379cf2470453179ad7a39
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-22 18:32:05 +0530

    Create product of two numbers.py

---
+++ product of two numbers.py
@@ -0,0 +1,6 @@
+Calculate the product of two numbers entered by the user.
+
+num1 = int(input("enter a first number"))
+num2 = int(input("enter a second number"))
+result = num1 + num2
+print(f"enter a first number {num1}, enter a second number{num2} addition { result}")
