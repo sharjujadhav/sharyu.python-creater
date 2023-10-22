044974a3ed00f93081ac4fe537faeb9dc1ea9b58
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 00:52:10 +0530

    Create circle segment.py

---
+++ circle segment.py
@@ -0,0 +1,6 @@
+42.Calculate the area of a circle segment
+
+import math as M
+radius = float(input("please enter the radius of the given circle:"))
+area_of_the_circle = M.pi*radius*radius
+print("the area of the given circle is:",area_of_the_circle)
