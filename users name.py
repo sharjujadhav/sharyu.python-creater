81cdc281c270875a6002e430e8ed315e5d3c284f
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-22 18:28:16 +0530

    Create users name.py

---
+++ users name.py
@@ -0,0 +1,5 @@
+1. Create a Python program that takes the user's name as input and then uses
+that input to greet the user personally.
+
+name = input("enter your name")
+print (f"Hello {name}, nice to meet you")n
