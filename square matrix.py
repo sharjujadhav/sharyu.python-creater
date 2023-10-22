70a03a131e1be203a9dbe2a0653ed059a6ab6183
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 01:05:42 +0530

    Create square matrix.py

---
+++ square matrix.py
@@ -0,0 +1,20 @@
+48.Find the determinant of a square matrix.
+
+
+def identicalmatrices(a,b):
+    if a==b:
+        print ("matrices are equal")
+    else:
+        print ("matrices are not equal")
+if __name__== "__main__":
+    a =[ [1, 1, 1, 1],
+       [2, 2, 2, 2],
+       [3, 3, 3, 3],
+       [4, 4, 4, 4]]
+    
+    b =[ [ 1, 1, 1, 1],
+        [2, 2, 2, 2],
+        [3, 3, 3, 3],
+        [4, 4, 4, 4]]
+    identicalmatrices(a,b)
+
