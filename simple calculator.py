219849bbe743b629e5b0110c93fbb63a511cb64f
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 00:03:15 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,16 @@
+28.Create a simple calculator that takes user input for two numbers and an
+operation (+, -, *, /).
+
+n1 = float(input("enter the first number:"))
+n2 = float(input("enter the second number:"))
+print("{} + {} = ".format(n1,n2))
+print(n1+n2)
+
+print("{} - {} = ".format(n1,n2))
+print(n1-n2)
+
+print("{} * {} = ".format(n1,n2))
+print(n1*n2)
+
+print("{} / {} = ".format(n1,n2))
+print(n1/n2)
