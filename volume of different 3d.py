9c0cd7846d5ea55f096dc0e903a0f82c9070d630
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 00:46:21 +0530

    Create volume of different 3d.py

---
+++ volume of different 3d.py
@@ -0,0 +1,24 @@
+39.Calculate the volume of different 3D shapes (cube, sphere, cylinder
+
+
+
+
+cube_volume = cube_side_length ** 3
+sphere_volume = (4/3) * math.pi 
+sphere_radius ** 3
+cylinder_volume = math.pi 
+cylmport math
+
+cube_side_length = 5
+
+sphere_radius = 3
+
+cylinder_radius = 2 
+
+cylinder_height = 6inder_radius **2
+cylinder_height
+
+print(f"cube volume: {cube _volume:.2f}")
+print(f"sphere volum: {sphere_volume:. 2f}")
+print(f"cylinder volume: {cylinder_volume: 2f}")
+                                                
