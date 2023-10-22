5679112ad72ec2560f07fb1ed02d3cc07199497b
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-22 23:27:39 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+16.Write a program to check if a number is a palindrome.
+
+um = input("enter a number")
+if num ==num[::-1]:
+    print("yes its a palindrome")
+else:
+    print("no its not a palindrome")
+
