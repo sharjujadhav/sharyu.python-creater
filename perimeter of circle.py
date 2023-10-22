588d003bf10087383d869e1663a71d183b526dea
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-22 23:05:21 +0530

    Create perimeter of circle.py

---
+++ perimeter of circle.py
@@ -0,0 +1,7 @@
+8. Calculate the perimeter of a circle given its radius
+
+
+radius = float(input("enter a radius"))
+print(f"radius is {radius}")
+parameter = 2*3.14 * radius
+print(f" parameter of circle is {parameter}")
