a6b44e4c9d41c6962c38af83f085fd16e13fb10a
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 01:04:34 +0530

    Create submatrix.py

---
+++ submatrix.py
@@ -0,0 +1,10 @@
+47.Extract a submatrix from a larger matrix.
+
+
+import numpy as np
+from numpy import linalg
+matrix = np.array([[1, 0],[3, 6]])
+print("original 2-d mattrix")
+print(matrix)
+print("determinant of the 2-d matrix:")
+print(np. linalg.det(matrix))
