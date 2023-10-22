0b5d11baac84dfc12a6dc1035d2b6ce9ab22ea54
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 01:03:12 +0530

    Create matrix add and sub.py

---
+++ matrix add and sub.py
@@ -0,0 +1,7 @@
+46.Implement matrix addition and subtraction.
+
+import numpy as np 
+arr = np.arange(16).reshape(4,4)
+print("origanal matrix:\n",arr,"\n")
+res = arr[np.ix_([0,3],[0,3])]
+print("created submatrix:\n",res,"\n")
