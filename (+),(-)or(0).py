20f0e264f0ed0a343de1c8fe3f3a3533062d50fc
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-22 23:09:13 +0530

    Create (+),(-)or(0).py

---
+++ (+),(-)or(0).py
@@ -0,0 +1,10 @@
+10.Write a program to check if a number is positive, negative, or zero
+
+num1 = int(input("enter a first number"))
+
+if num1>0:
+    print(f"{num1} is positive")
+elif num==0:
+    print(f"{num} is zero")
+else:
+    print(f"{num2} is negative")
