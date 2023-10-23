ebbf195339d181ea9148695b80ef919a133d93d1
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 23:17:09 +0530

    Create average value.py

---
+++ average value.py
@@ -0,0 +1,9 @@
+57.Calculate the average value of elements in a list using a set
+
+def Average(lst): 
+    return sum(lst) / len(lst) 
+ 
+lst = [15, 9, 55, 41, 35, 20, 62, 49] 
+average = Average(lst) 
+ 
+print("Average of the list =", round(average, 2)
