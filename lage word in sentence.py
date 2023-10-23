be68a2068d9e3d8003d03e296f968b14a6c0c36c
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-23 23:31:52 +0530

    Create lage word in sentence.py

---
+++ lage word in sentence.py
@@ -0,0 +1,15 @@
+61.Find the largest word in a sentence.
+
+def largestWord(s):
+ 
+      s = sorted(s, key = len)
+ 
+        print(s[-1])
+ 
+ if __name__ == "__main__":
+ 
+      s = "be confident and be yourself"
+ 
+    l = list(s.split(" "))
+ 
+    largestWord(l)
