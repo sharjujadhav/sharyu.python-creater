2f73c667a1eb96383b5d0955e74a3d98e09af66b
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-24 00:46:26 +0530

    Create 2 list are equal.py

---
+++ 2 list are equal.py
@@ -0,0 +1,20 @@
+80.Check if two lists are equal (have the same elements in the same order).
+
+
+l1 = [10, 20, 30, 40, 50] 
+l2 = [20, 30, 50, 40, 70] 
+l3 = [50, 10, 30, 20, 40] 
+
+l1.sort() 
+l2.sort() 
+l3.sort() 
+
+if l1 == l2: 
+    print ("The lists l1 and l2 are the same") 
+else: 
+    print ("The lists l1 and l2 are not the same") 
+
+if l1 == l3: 
+    print ("The lists l1 and l3 are the same") 
+else: 
+    print ("The lists l1 and l3 are not the same")
