d212d6178fdc232fa1a424d76a319c832d45b92e
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-24 00:19:56 +0530

    Create single str.py

---
+++ single str.py
@@ -0,0 +1,12 @@
+71.Convert a list of strings into a single string.
+
+  def listToString(s):
+ 
+      str1 = ""
+ 
+      for ele in s:
+        str1 += ele
+ 
+        return str1
+s = ['Geeks', 'for', 'Geeks']
+print(listToString(s))
