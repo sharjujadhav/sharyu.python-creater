168d58ebe9b9e731e8a01c6ad87775890f5c01bd
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-24 00:08:54 +0530

    Create common keys.py

---
+++ common keys.py
@@ -0,0 +1,7 @@
+68.Merge two dictionaries while summing values for common keys.
+
+  dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 4, 'd': 5}
+
+merged_dict = {**dict1, **dict2}
+print(merged_dict) 
