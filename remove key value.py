93198656215b97a68fc8da37182805c6ac477040
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-24 00:33:07 +0530

    Create remove key value.py

---
+++ remove key value.py
@@ -0,0 +1,11 @@
+76.Remove a key-value pair from a dictionary
+
+test_dict = {"Arushi": 22, "Mani": 21, "Haritha": 21}
+ 
+print("The dictionary before performing remove is : ", test_dict)
+ 
+del test_dict['Mani']
+ 
+print("The dictionary after remove is : ", test_dict)
+  dict
+del test_dict['Mani']
