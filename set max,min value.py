bd142710f217267df12f5ba983acf4b8fdfb5f36
Sharyujadhav <146825461+sharjujadhav@users.noreply.github.com>
2023-10-24 00:28:48 +0530

    Create set max,min value.py

---
+++ set max,min value.py
@@ -0,0 +1,12 @@
+74.Convert a list of integers into a set and find the maximum and minimum
+values
+
+my_set = {5, 2, 8, 1, 9, 2, 18}
+
+my_list = list(my_set)
+
+maximum = max(my_list)
+minimum = min(my_list)
+
+print("Maximum:", maximum)
+print("Minimum:", minimum)
